3b54603874bfaf6750f8768e1a469518b8bedbdc
3amrA7med <amr.ahmed.abdelbaqi@gmail.com>
2020-12-23 15:18:52 +0200

    Fixed copying all the color domain in temp variable bug in backtracking with forward checking algorithm, changed the testing graph to test the backtraing

--- algorithms/backtracking_with_forward_checking.py
+++ algorithms/backtracking_with_forward_checking.py
@@ -12,26 +12,24 @@ def backtracking_with_forward_checking_rec(index, number_of_nodes, graph, color_
     if index == number_of_nodes:
         return True, color_assignment
 
-    color_domain_of_this_node = color_assignment[index].copy()
+    color_domain_of_this_node = color_assignment.copy()
     for color in color_assignment[index]:
-        # Check colors one by one and if safe assign to the node and move to next node
-        if is_safe_assignment(index, graph, color, color_assignment):
-            # Assign the color the node
-            color_assignment[index] = [color]
-            # Forward checking step
-            inferences, fc_color_assignment = forward_checking(index, graph, color, color_assignment)
-            if inferences:
-                # Set the new color assignment to the values obtained from forward checking
-                color_assignment = fc_color_assignment
-                # Move to next nodes
-                solution_found, bt_color_assignment = \
-                    backtracking_with_forward_checking_rec(index+1, number_of_nodes, graph, color_assignment,
-                                                           k_coloring)
-                # If solution found return it
-                if solution_found:
-                    return True, bt_color_assignment
-                else:
-                    color_assignment[index] = color_domain_of_this_node.copy()
+        # Assign the color the node
+        color_assignment[index] = [color]
+        # Forward checking step
+        inferences, fc_color_assignment = forward_checking(index, graph, color, color_assignment)
+        if inferences:
+            # Set the new color assignment to the values obtained from forward checking
+            color_assignment = fc_color_assignment
+            # Move to next nodes
+            solution_found, bt_color_assignment = \
+                backtracking_with_forward_checking_rec(index+1, number_of_nodes, graph, color_assignment,
+                                                       k_coloring)
+            # If solution found return it
+            if solution_found:
+                return True, bt_color_assignment
+            else:
+                color_assignment = color_domain_of_this_node.copy()
     # This means that there is no solution exists
     return False, color_assignment
 
@@ -49,15 +47,3 @@ def forward_checking(index_of_the_node, graph, color_assigned_to_node, color_ass
                 return False, []
     # All neighbors have colors left in their domains.
     return True, color_assignment
-
-
-def is_safe_assignment(index_of_the_node, graph, color_assigned_to_node, color_assignment):
-    """Implements function is safe that make sure that the current assignment is safe(free of conflicts)"""
-    # Loop on all neighbors of the node assigned and check if the value assigned to the neighbor
-    # is similar to the value assigned to the node, if so return false
-    for neighbor in graph[index_of_the_node]:
-        # Check if my neighbor already assigned to this color
-        if len(color_assignment[neighbor]) == 1:
-            if color_assignment[neighbor][0] == color_assigned_to_node:
-                return False
-    return True
--- mc_solver.py
+++ mc_solver.py
@@ -7,7 +7,7 @@ from algorithms import backtracking, min_conflicts, backtracking_with_forward_ch
               help='Algorithm type.', required=True, default='bt')
 @click.option('-k', '--k_coloring', required=True,  type=click.types.INT,
               help='Number of colors to solve this problem(only 3 and 4).', default=3)
-@click.option('-n', '--number_of_nodes', default=7,
+@click.option('-n', '--number_of_nodes', default=6,
               help='Number of nodes used in the map coloring', type=click.types.INT, required=True)
 def solver(algorithm, k_coloring, number_of_nodes):
     """Solver to map coloring problem"""
@@ -16,27 +16,27 @@ def solver(algorithm, k_coloring, number_of_nodes):
 
     # Define adjacency list graph to test it
     graph = [
-        [1, 2],
-        [0, 2, 3],
-        [0, 1, 3, 4, 5],
-        [1, 2, 4],
+        [1, 5],
+        [0, 2, 5],
+        [1, 3, 4],
+        [2, 4],
         [2, 3, 5],
-        [2, 4]
+        [0, 1, 4]
     ]
     if algorithm == "bt":
-        solution_exits, answer = backtracking(6, graph, k_coloring)
+        solution_exits, answer = backtracking(number_of_nodes, graph, k_coloring)
         if solution_exits:
             print(answer)
         else:
             print("No Solution exits.")
     elif algorithm == "mc":
-        answer = min_conflicts(graph, 6, k_coloring, 40)
+        answer = min_conflicts(graph, number_of_nodes, k_coloring, 40)
         if answer:
             print(answer)
         else:
             print("No Solution exits.")
     elif algorithm == "bt-fc":
-        solution_exits, answer = backtracking_with_forward_checking(6, graph, k_coloring)
+        solution_exits, answer = backtracking_with_forward_checking(number_of_nodes, graph, k_coloring)
         if solution_exits:
             print(answer)
         else:
